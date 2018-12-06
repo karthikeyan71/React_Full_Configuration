11686eb59ebe61c298cd133c7acbf53567414382
karthikeyan <karthikeyanm.official@gmail.com>
2018-12-06 11:40:28 +0530

    feat: 🎸 configurations for the es6

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import styles from './styles.css';
+import { add, subtract } from './module';
+
+const resultA = add(2, 3);
+const resultB = subtract(5, 1);
+console.log(resultA, resultB);
+console.log(styles.localClass);
+console.log(styles.globalClass);
---
+++ webpack.config.babel.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import webpack from 'webpack';
+
+console.log(process.env.APP_NAME);
+
+export default {
+  devtool: 'eval-source-map',
+  entry: path.join(process.cwd(), 'src/index'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(process.cwd(), 'public', 'js'),
+    publicPath: '/js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['transform-inline-environment-variables'],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s)?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: 'file-loader',
+      },
+    ],
+  },
+  target: 'web',
+};
